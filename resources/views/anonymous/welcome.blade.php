5a255379045bcff907cc240d3ec1c71f793149c1
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-03-16 11:17:37 +0100

    [EDIT] button's css

--- resources/views/anonymous/welcome.blade.php
+++ resources/views/anonymous/welcome.blade.php
@@ -3,7 +3,7 @@
 @section("content")
     <div style="width: 60%">
         Hello World !
-        <div class="todo-btn-round">
+        <div class="todo-btn-secondary">
             <p>S'inscrire</p>
         </div>
     </div>
