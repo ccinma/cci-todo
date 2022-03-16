385829fb4f2587539532cb136e099565977ebb71
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-03-16 11:20:25 +0100

    [CLEAN] welcome.blade

--- resources/views/anonymous/welcome.blade.php
+++ resources/views/anonymous/welcome.blade.php
@@ -3,8 +3,5 @@
 @section("content")
     <div style="width: 60%">
         Hello World !
-        <div class="todo-btn-secondary">
-            <p>S'inscrire</p>
-        </div>
     </div>
 @endsection
