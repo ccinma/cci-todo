79e39f81b9651ddc4b175eadc9d88f5f5e2b254e
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-03-16 10:47:00 +0100

    [EDIT] button's css

--- resources/views/anonymous/welcome.blade.php
+++ resources/views/anonymous/welcome.blade.php
@@ -1,7 +1,10 @@
 @extends("layouts.components.containers._bigContainer")
 
 @section("content")
-    <div>
+    <div style="width: 60%">
         Hello World !
+        <div class="todo-btn-round">
+            <p>S'inscrire</p>
+        </div>
     </div>
 @endsection
