f663c5d91c547c5c1c436c787b4759320a19d8d9
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-03-18 16:09:10 +0100

    [EDIT] of all of scss

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,14 +4,17 @@
 <div id="register">
     <h3>Ravis de vous accueillir parmis nous</h3>
     <div class="hr"></div>
-    @include("layouts.components.svg.small_logo")
 
-    <div class="form">
-        <form method="POST" action="{{ route('register') }}">
+    <div class='svg-container'>
+        @include("layouts.components.svg.small_logo")
+    </div>
+
+    <div class="todo-form-container">
+        <form class="todo-form" method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div class="email">
-                <input class="todo-form" placeholder="Identifiant" id="name" type="text" @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <div class="todo-form-group first">
+                <input class="todo-form-text" placeholder="Identifiant" id="name" type="text" @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                 @error('name')
                     <span role="alert">
@@ -20,8 +23,8 @@
                 @enderror
             </div>
 
-            <div>
-                <input class="todo-form" placeholder="Email" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <div class="todo-form-group">
+                <input class="todo-form-text" placeholder="Email" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                 @error('email')
                     <span role="alert">
@@ -30,8 +33,8 @@
                 @enderror
             </div>
 
-            <div>
-                <input class="todo-form" placeholder="Mot de passe" id="password" type="password" name="password" required autocomplete="new-password">
+            <div class="todo-form-group">
+                <input class="todo-form-text" placeholder="Mot de passe" id="password" type="password" name="password" required autocomplete="new-password">
 
                 @error('password')
                     <span role="alert">
@@ -40,11 +43,11 @@
                 @enderror
             </div>
 
-            <div>
-                <input class="todo-form" placeholder="Confirmer le mot de passe" id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
+            <div class="todo-form-group">
+                <input class="todo-form-text" placeholder="Confirmer le mot de passe" id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
             </div>
 
-            <div>
+            <div class="todo-form-group">
                 <button class="todo-btn-round" type="submit">
                     S'inscrire
                 </button>
@@ -52,6 +55,9 @@
         </form>
     </div>
     <div class="hr"></div>
-    <a class="login" href="{{ route('login') }}">Vous avez déjà un compte ? C'est par ici !</a>
+
+    <div class="link-container">
+        <a class="login" href="{{ route('login') }}">Vous avez déjà un compte ? C'est par ici !</a>
+    </div>
 </div>
 @endsection
