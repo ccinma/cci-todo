06c09824b55de810fd600fdc6b6a04e12733fd5e
Nezertiam <mawyn.nhek@gmail.com>
2022-03-23 16:35:53 +0100

    [FIX] Update board

--- app/Http/Controllers/Board/UpdateBoardController.php
+++ app/Http/Controllers/Board/UpdateBoardController.php
@@ -6,21 +6,23 @@ use App\Board;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateBoardRequest;
 use Auth;
+use Str;
 
 class UpdateBoardController extends Controller
 {
-    public function update($board, UpdateBoardRequest $request)
+    public function update($board_id, UpdateBoardRequest $request)
     {
-        $board = Board::with(['workspace', 'workspace.members'])->find($board);
-        $attributes = $request->validated();
-        
-        if ( ! $board ) {
-            return response()->json([], 404);
+        if ( ! Str::isUuid($board_id) ) {
+            return response()->json([], 400);
         }
+
+        $board = Board::findOrFail($board_id);
 
         if ( ! $board->workspace->hasMember(Auth::user()) ) {
             return response()->json([], 401);
         }
+
+        $attributes = $request->validated();
 
         if ( empty($attributes) ) {
             return response()->json([], 304);
@@ -28,7 +30,9 @@ class UpdateBoardController extends Controller
 
         $board->update($attributes);
 
-        return response()->json(['data' => $board]);
+        return response()->json([
+            'data' => $board->withoutRelations(),
+        ], 200);
 
     }
 }
