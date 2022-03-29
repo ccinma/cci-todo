34b489f41fda414d34b8fac1ed879938cc44306b
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-03-29 18:30:27 +0200

    [ADD] btn for close and open left board

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="all_container">
+  <div id="dashboard">
+    <div class="all_container left" v-bind:class="{ active: leftIsOpen, 'all_container left-close': !leftIsOpen }">
       <div class="top">
         <h3>{{ workspace.name }}</h3>
         <div class="hr"></div>
@@ -17,9 +17,22 @@
             </li>
           </ul>
         </div>
+
+        <div class="hr"></div>
+
+        <div class="users">
+          <div class="title">
+            <p>Participants</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>
+          </div>
+
+          <ul>
+            <li v-for="item in workspace.users" :key="item.name">
+              {{ item.name }}
+            </li>
+          </ul>
+        </div>
       </div>
-      
-      
       
       <div class="bottom">
         <div class="hr"></div>
@@ -28,6 +41,14 @@
           <p>Changer d'espace de travail</p>
         </div>
       </div>
+    </div>
+    
+    <div class="all_container right">
+      <button class="todo-btn-secondary-round btn" v-bind:class="{ active: leftIsOpen, 'btn-flip': !leftIsOpen }" v-on:click="changeLeft">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg>      
+        </div>
+      </button>
     </div>
   </div>
 </template>
@@ -42,71 +63,138 @@
             {name: "Front-End"},
             {name: "Back-End"},
           ],
-        }
+          users: [
+            {name: "Pierre"},
+            {name: "Paul"},
+            {name: "Jack"},
+          ]
+        },
+
+        leftIsOpen: true,
       }
     },
 
+    methods: {
+      changeLeft(event) {
+        if (this.leftIsOpen) {
+          this.leftIsOpen = false;
+        }
+        else {
+          this.leftIsOpen = true;
+        }
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   @import "../../sass/app.scss";
 
-  .all_container {
-    max-width: 15rem;
-    // overflow-y: scroll;
-
+  #dashboard {
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    // flex-direction: row;
 
-    .top {
-      h3 {
-        margin-block: 2rem;
-        font-size: 1.5rem;
-        text-align: center;
+    .all_container {
+      z-index: 0;
+    }
+
+    .left {
+      max-width: 15rem;
+      margin-right: 2rem;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .top {
+        h3 {
+          margin-block: 2rem;
+          font-size: 1.5rem;
+          text-align: center;
+        }
+
+        .board, .users {
+          margin-block: 2rem;
+
+          .title {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+
+            p {
+              font-weight: bold;
+              font-size: 1.2rem;
+            }
+
+            svg {
+              height: 1rem;
+              fill: $font-color-primary;
+            }
+          }
+        }
       }
 
-      .board {
-        margin-block: 2rem;
-
-        .title {
+      .bottom {
+        .change {
+          margin-top: 2rem;
           display: flex;
           flex-direction: row;
           justify-content: space-between;
           align-items: center;
 
-          p {
-            font-weight: bold;
-            font-size: 1.2rem;
-          }
-
           svg {
             height: 1rem;
+            margin-right: 1rem;
             fill: $font-color-primary;
           }
         }
       }
-    }
 
-    .bottom {
-      .change {
-        margin-top: 2rem;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-
-        svg {
-          height: 1rem;
-          fill: $font-color-primary;
+      @include tablet {
+        h3 {
+          font-size: 2rem;
         }
       }
     }
 
-    @include tablet {
-      h3 {
-        font-size: 2rem;
+    .left-close {
+      @extend .left;
+      width: 0;
+      margin-right: 0;
+      padding: 0;
+
+      * {
+        display: none;
+      }
+    }
+
+    .right {
+      width: 100%;
+
+      .btn {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+
+        width: auto;
+
+        margin: 2rem;
+        padding: 0.75rem;
+
+        > div {
+          width: 1rem;
+          height: 1rem;
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+
+      .btn-flip {
+        @extend .btn;
+        transform: rotate(0.5turn);
       }
     }
   }
