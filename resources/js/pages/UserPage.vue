54a777807c7d2f4faf8b41a8faef82cba767e9df
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-04-26 14:23:26 +0200

    feat:add profile picture template

--- resources/js/pages/UserPage.vue
+++ resources/js/pages/UserPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="choose-workspace">
     <div class="container">
-      <h2>Liste des espaces de travail</h2>
+      <h2>Page de profile</h2>
+      <div class="profile">
+        <div class="profile-pic-container">
+          <img :src="imageSrc" alt="Photo de profile">
+        </div>
+        <p>{{ userName}}</p>
+      </div>
+
+      <h3>Liste des espaces de travail</h3>
       <ul v-if=" workspaces && workspaces.length > 0 ">
         <li v-for="(workspace, index) in workspaces" v-bind:key="'workspace-list-element-'+index" v-on:click.prevent="setWorkspace(workspace.id)">
           <router-link :to="'/workspace/'+workspace.id">
@@ -10,7 +18,7 @@
         </li>
       </ul>
       <a v-on:click.prevent="openNewWorkspacePopup()">
-        + Ajouter un espace de travail
+        + Créer un nouvel espace de travail
       </a>
     </div>
   </div>
@@ -18,9 +26,12 @@
 
 <script>
 export default {
-  name: 'ChooseWorkspace',
+  name: 'UserPage',
   data() {
     return {
+      imageSrc: "https://img-19.commentcamarche.net/cI8qqj-finfDcmx6jMK6Vr-krEw=/1500x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg",
+      userName: this.$store.getters.user().name,
+
       workspaces: this.$store.getters.workspaces(),
       currentWorkspace: this.$store.getters.currentWorkspace(),
     }
@@ -64,6 +75,34 @@ export default {
     border-radius: 1rem;
 
     padding: 1rem;
+
+    .profile {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      margin-bottom: 1rem;
+      .profile-pic-container {
+        width: 10rem;
+        height: 10rem;
+
+        border-radius: 99rem;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+
+          object-position: center center;
+          object-fit: cover;
+        }
+      }
+
+      p {
+        font-size: 1.5rem;
+        margin-top: 0.5rem;
+      }
+    }
 
     h2 {
       margin-bottom: 1rem;
